e8d49a9214f3c92913f18b160ce4aeefe2c089ae
Mandy <Lmandy239@gmail.com>
2024-04-09 23:46:09 -0400

    2 drills

--- src/JavaDrill.java
+++ src/JavaDrill.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
 
 public class JavaDrill {
-
+	public static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		JavaDrill jd = new JavaDrill();
+		
+		
+		// Circle area
+		System.out.println(jd.circleArea(5));
+		System.out.println("\n\n");
+		//Reverse a String
+		System.out.println("Enter a word");
+		String word = sc.nextLine();
+		jd.reverseString(word);
+		
+	}
+	public double circleArea(int radius) {
+		double area = (radius * radius) * Math.PI;
+		return area;
+	}
+	
+	public void reverseString(String word) {
+		StringBuilder sb = new StringBuilder(word).reverse();	
+		System.out.println(sb);
+	}
 }
